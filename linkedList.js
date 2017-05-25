1b8a72bb45e08d67be272d01efb9039920d13720
James Moore <jameslmoore@gmail.com>
2017-05-24 21:13:34 -1000

    gethead and working on add

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,55 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var head = null;
 
+  function getHead(){
+    return head;
+  }
+
+  function getTail(){
+
+  }
+
+  function add(val){
+   if (head === null){
+    head = {value: val,
+            next: null};
+            console.log("head is " +head.next);
+
+  }else{
+    console.log("adsf");
+  }
 }
+  // head = {value: val,
+  //         next: null,
+  //         _length : 0};
+  // }
+  function remove(){
+  }
+
+  function get(){
+  }
+
+  function insert(){
+  }
+
+
+  return {
+    getHead,
+    getTail,
+    add,
+    remove,
+    get,
+    insert
+  };
+}
+
+var myLL = linkedListGenerator();
+
+myLL.add("james");
+myLL.add("dory");
+console.log(myLL.getHead().next);
+
+
+//for (var i = 0; i<)
